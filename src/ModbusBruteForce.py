b9907cd0f816ee9647405f8bd542ebb279f06964
1mdonk3y <67701333+sudo-JACT@users.noreply.github.com>
2021-08-05 10:05:41 +0200

    Update ModbusBruteForce.py

--- src/ModbusBruteForce.py
+++ src/ModbusBruteForce.py
@@ -1,42 +1,111 @@
 from pyModbusTCP.client import ModbusClient
+from rich.console import Console
+from art import *
 
-client=ModbusClient(host="127.0.0.1", port=12345) #--> specify the host and port where the client connect
+console = Console()
+
+asci=tprint("ModBus Brute Force",font="random")
+
+host=console.input(f"[bold green]HOST:[/bold green] ")
+
+port=console.input(f"[bold green]PORT:[/bold green] ")
+
+ad=console.input(f"[bold green]MAX ADDRESS:[/bold green] ")
+ad=int(ad)
+
+ID=console.input(f"[bold green]UNIT ID:[/bold green] ")
+
+if ID == "" or 0:
+    ID=1
+
+client=ModbusClient(host=host, port=port, auto_open=True, auto_close=True, timeout=10)
 
 
-#open connection
+client.unit_id(ID)
 b=client.open()
 
-print(b) #--> print the connection status True if the connection is ok else return False
 
-global a
 
-a=0
+global addr
 
-#for loop
-for x in range(256):
-    if a > 0:
-        v=client.read_holding_registers(a,125) #holding registers data
-        k=client.read_input_registers(a,125) #input registers data
-        l=client.read_discrete_inputs(a,2000) #discrete inputs data
-        print(f"Address: {a}\nHolding Registers Data: {v}\nInput Registers Data: {k}\nDiscrete Inputs Data: {l}\nConnection: {b}\n-------------------\n") #print in the console
+addr=0
+
+for x in range(ad):
+    if addr == ad:
+        v=client.read_holding_registers(addr,2)
+        k=client.read_input_registers(addr,2)
+        l=client.read_discrete_inputs(addr,2)
+        err=client.last_error_txt()
+        exx=client.last_except_txt(verbose=True)
+        if b != False and v != None:
+            
+            console.print(f"Address: {addr}\nHolding Registers Data: [bold green]{v}[/bold green]\nInput Registers Data: [bold green]{k}[/bold green]\nDiscrete Inputs Data: [bold green]{l}[/bold green]\nConnection: [bold green]{b}[/bold green]\n-------------------\n")
+            
+            file=open("log.txt","a+")
+            file.write(f"Address: {addr}\nHolding Registers Data: {v}\nInput Registers Data: {k}\nDiscrete Inputs Data: {l}\nConnection: {b}\n-------------------\n")
+            file.close()
+            
+            
+        else:
+            console.print(f"Address: {addr}\nHolding Registers Data: [bold red]{v}[/bold red]\nInput Registers Data: [bold red]{k}[/bold red]\nDiscrete Inputs Data: [bold red]{l}[/bold red]\nConnection: [bold red]{b}[/bold red]\nError: [bold red]{err}[/bold red]\nExcept: [bold red]{exx}[/bold red]\n-------------------\n")
+            
+            file=open("log.txt","a+")
+            file.write(f"Address: {addr}\nHolding Registers Data: {v}\nInput Registers Data: {k}\nDiscrete Inputs Data: {l}\nConnection: {b}\nError: {err}\nExcept: {exx}\n-------------------\n")
+            file.close()
+        
+        break
+    
+    if addr > 0:
+        v=client.read_holding_registers(addr,2)
+        k=client.read_input_registers(addr,2)
+        l=client.read_discrete_inputs(addr,2)
+        err=client.last_error_txt()
+        exx=client.last_except_txt(verbose=True)
+        
+        if b != False and v != None:
+            
+            console.print(f"Address: {addr}\nHolding Registers Data: [bold green]{v}[/bold green]\nInput Registers Data: [bold green]{k}[/bold green]\nDiscrete Inputs Data: [bold green]{l}[/bold green]\nConnection: [bold green]{b}[/bold green]\n-------------------\n")
+            
+            file=open("log.txt","a+")
+            file.write(f"Address: {addr}\nHolding Registers Data: {v}\nInput Registers Data: {k}\nDiscrete Inputs Data: {l}\nConnection: {b}\n-------------------\n")
+            file.close()
+            
+            
+        else:
+            console.print(f"Address: {addr}\nHolding Registers Data: [bold red]{v}[/bold red]\nInput Registers Data: [bold red]{k}[/bold red]\nDiscrete Inputs Data: [bold red]{l}[/bold red]\nConnection: [bold red]{b}[/bold red]\nError: [bold red]{err}[/bold red]\nExcept: [bold red]{exx}[/bold red]\n-------------------\n")
+            
+            file=open("log.txt","a+")
+            file.write(f"Address: {addr}\nHolding Registers Data: {v}\nInput Registers Data: {k}\nDiscrete Inputs Data: {l}\nConnection: {b}\nError: {err}\nExcept: {exx}\n-------------------\n")
+            file.close()
+            
+            
        
         
-        file=open("log.txt","a+")
-        file.write(f"Address: {a}\nHolding Registers Data: {v}\nInput Registers Data: {k}\nDiscrete Inputs Data: {l}\nConnection: {b}\n-------------------\n") #save in a file
-        file.close()
+        
     else:
-        v=client.read_holding_registers(a,125)
-        k=client.read_input_registers(a,125)
-        l=client.read_discrete_inputs(a,2000)
-        print(f"Address: {a}\nHolding Registers Data: {v}\nInput Registers Data: {k}\nDiscrete Inputs Data: {l}\nConnection: {b}\n-------------------\n") #print in the console
+        v=client.read_holding_registers(addr,2)
+        k=client.read_input_registers(addr,2)
+        l=client.read_discrete_inputs(addr,2)
+        err=client.last_error_txt()
+        exx=client.last_except_txt(verbose=True)
+        
+        if b != False and v != None:
+            console.print(f"Address: {addr}\nHolding Registers Data: [bold green]{v}[/bold green]\nInput Registers Data: [bold green]{k}[/bold green]\nDiscrete Inputs Data: [bold green]{l}[/bold green]\nConnection: [bold green]{b}[/bold green]\n-------------------\n")
+            
+            file=open("log.txt","w")
+            file.write(f"Address: {addr}\nHolding Registers Data: {v}\nInput Registers Data: {k}\nDiscrete Inputs Data: {l}\nConnection: {b}\n-------------------\n")
+            file.close()
+            
+            
+        else:
+            console.print(f"Address: {addr}\nHolding Registers Data: [bold red]{v}[/bold red]\nInput Registers Data: [bold red]{k}[/bold red]\nDiscrete Inputs Data: [bold red]{l}[/bold red]\nConnection: [bold red]{b}[/bold red]\nError: [bold red]{err}[/bold red]\nExcept: [bold red]{exx}[/bold red]\n-------------------\n")
+            
+            file=open("log.txt","w")
+            file.write(f"Address: {addr}\nHolding Registers Data: {v}\nInput Registers Data: {k}\nDiscrete Inputs Data: {l}\nConnection: {b}\nError: {err}\nExcept: {exx}\n-------------------\n")
+            file.close()
         
         
-        file=open("log.txt","w")
-        file.write(f"Address: {a}\nHolding Registers Data: {v}\nInput Registers Data: {k}\nDiscrete Inputs Data: {l}\nConnection: {b}\n-------------------\n") #save in a file
-        file.close()
-        
-        
-    a+=1
+    addr+=2
 
     
     
